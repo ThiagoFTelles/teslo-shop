ff99b0e7e6288365ec40b99ecf62bcc8c8f65a09
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-21 20:47:08 -0300

    feat: add pagination dto

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query } from '@nestjs/common';
 
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -15,8 +16,8 @@ export class ProductsController {
 
   // TODO: paginate
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.productsService.findAll( paginationDto );
   }
 
   @Get(':term')
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,8 +1,10 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 
 import { Product } from './entities/product.entity';
 
@@ -25,8 +27,13 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return this.productRepository.find()
+  findAll( paginationDto: PaginationDto ) {
+    const { limit = 10, offset = 0 } = paginationDto
+    return this.productRepository.find({
+      take: limit,
+      skip: offset,
+      //TODO: relashionships
+    })
   }
 
   async findOne(term: string) {
