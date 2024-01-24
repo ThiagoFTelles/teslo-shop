a9d67cd95579f52ea84f772724006e5f192e4193
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-23 23:34:36 -0300

    feat: return product images

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -35,13 +35,19 @@ export class ProductsService {
     }
   }
 
-  findAll( paginationDto: PaginationDto ) {
+  async findAll( paginationDto: PaginationDto ) {
     const { limit = 10, offset = 0 } = paginationDto
-    return this.productRepository.find({
+    const products = await this.productRepository.find({
       take: limit,
       skip: offset,
-      //TODO: relashionships
+      relations: {
+        images: true,
+      }
     })
+    return products.map( ({ images, ...rest }) => ({
+      ...rest, 
+      images: images.map( image => image.url )
+    }))
   }
 
   async findOne(term: string) {
@@ -49,18 +55,28 @@ export class ProductsService {
     if( isUUID(term) ) {
       product = await this.productRepository.findOneBy({id: term})
     } else {
-      const queryBuilder = this.productRepository.createQueryBuilder()
+      const queryBuilder = this.productRepository.createQueryBuilder('prod') // 'prod' = product table alias
       product = await queryBuilder
         .where('UPPER(title) =:title or slug =:slug', {
           title: term.toUpperCase(),
           slug: term.toLowerCase(),
-        }).getOne()
+        })
+        .leftJoinAndSelect('prod.images', 'prodImages')
+        .getOne()
     }
 
     if(!product) 
       throw new NotFoundException(`Product with ID, name or slug "${term}" not found`)
 
     return product
+  }
+
+  async findOnePlain( term: string ) {
+    const { images, ...rest } = await this.findOne( term )
+    return {
+      ...rest,
+      images: images.map(image => image.url)
+    }
   }
 
   async update(id: string, updateProductDto: UpdateProductDto) {
