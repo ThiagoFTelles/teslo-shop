2af026e2d32e8d780e69f1369a2abfb870babc40
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-29 21:26:18 -0300

    feat: delete all products before to seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsService } from './../products/products.service';
 
 @Injectable()
 export class SeedService {
+
+  constructor(
+    private readonly productsService: ProductsService
+  ) {}
+
   async runSeed() {
+    this.insertNewProducts()
     return 'Seed executed'
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllproducts()
+    return true
   }
 }
