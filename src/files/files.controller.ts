397724950cce316bbd5e1a4f4817e867871cabc2
ThiagoFTelles <atendimento.telles@gmail.com>
2024-01-31 22:41:02 -0300

    feat: add a file filter

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
   @Post('product')
-  uploadProductImage() {
-    return 'Product image uploaded'
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter
+  }) )
+  uploadProductImage(
+    @UploadedFile() file: Express.Multer.File
+    ) {
+
+      if ( !file ) {
+        throw new BadRequestException('Make sure that the file is an image.')
+      }
+
+      return {
+        fileName: file.originalname
+      }
   }
 }
