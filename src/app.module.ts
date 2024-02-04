72dc453ecd3b1f3d793755cafc5728a1fd849b27
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-04 18:15:27 -0300

    feat: serve the public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -19,6 +23,9 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: true, // TODO: Avoid this in production
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      }),
     ProductsModule,
     CommonModule,
     SeedModule,
