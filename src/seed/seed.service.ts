96b155c4c990334cdee91b33c2b1bea544123904
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-15 21:33:34 -0300

    feat: add users on feed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { User } from '../auth/entities/user.entity';
+
 import { ProductsService } from './../products/products.service';
 import { initialData } from './data/seed-data';
 
@@ -6,22 +11,48 @@ import { initialData } from './data/seed-data';
 export class SeedService {
 
   constructor(
-    private readonly productsService: ProductsService
+    private readonly productsService: ProductsService,
+    @InjectRepository( User )
+    private readonly userRepository: Repository<User>
   ) {}
 
   async runSeed() {
-    this.insertNewProducts()
+    await this.deleteTables()
+    const adminUser = await this.insertUsers()
+    await this.insertNewProducts( adminUser )
     return 'Seed executed'
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
+    await this.productsService.deleteAllproducts()
+    const queryBuilder = this.userRepository.createQueryBuilder()
+    await queryBuilder
+    .delete()
+    .where({})
+    .execute()
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users
+    const users: User[] = []
+
+    seedUsers.forEach(user => {
+      users.push( this.userRepository.create( user ) )      
+    });
+
+    const dbUsers = await this.userRepository.save( seedUsers )
+
+    return dbUsers[0]
+  }
+
+  private async insertNewProducts( user: User ) {
     await this.productsService.deleteAllproducts()
 
     const products = initialData.products
     const insertPromisses = []
 
     products.forEach( product => {
-      insertPromisses.push( this.productsService.create( product ) )
+      insertPromisses.push( this.productsService.create( product, user ) )
     } )
 
     await Promise.all( insertPromisses )
