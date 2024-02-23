218f0eb423bb2715e1b0bed40628fe649708a8f3
ThiagoFTelles <atendimento.telles@gmail.com>
2024-02-23 10:57:34 -0300

    feat: receive message from client

--- src/messages-ws/interfaces/EventNames.interface.ts
+++ src/messages-ws/interfaces/EventNames.interface.ts
@@ -1,3 +1,4 @@
 export enum EventNames {
     clientsUpdated = 'clients-updated',
+    messageFromClient = 'message-from-client',
 }
--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,9 +1,10 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 
 import { Server, Socket } from 'socket.io';
 
 import { MessagesWsService } from './messages-ws.service';
 import { EventNames } from './interfaces';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true, namespace: '/' })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -16,8 +17,14 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
     this.messagesWsService.registerClient( client )
     this.wss.emit(EventNames.clientsUpdated, this.messagesWsService.getConnectedClients())
   }
+
   handleDisconnect( client: Socket ) {
     this.messagesWsService.removeClient( client.id )
     this.wss.emit(EventNames.clientsUpdated, this.messagesWsService.getConnectedClients())
+  }
+  
+  @SubscribeMessage(EventNames.messageFromClient)
+  onMessageFromClient( client: Socket, payload: NewMessageDto ) {
+    console.log(client.id, payload)
   }
 }
